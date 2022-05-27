7e470fc0c338449477a5c1f49a49ff97712fc492
Hsinnii <ginny0312@gmail.com>
2022-05-27 16:16:16 +0800

    0527

--- management.php
+++ management.php
@@ -1,10 +1,4 @@
-<?php 
-    session_start();
-    $user_name = $_SESSION['user_name'];
-?>
- 
 <!DOCTYPE html>
-
 <html lang="en">
 
 <head>
@@ -22,30 +16,23 @@
     <!-- Load fonts style after rendering the layout styles -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;200;300;400;500;700;900&display=swap">
     <link rel="stylesheet" href="assets/css/fontawesome.min.css">
-    
-    <!-- jq -->
+<!-- jq -->
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.0/css/jquery.dataTables.min.css">
     <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script type="text/javascript" charset="utf8"
         src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
 
+    
+
+
 
     <script>
-        $(document).ready(function () {
+    $(document).ready(function () {
 
-        $('#管理帳號').DataTable();
-        });
+      $('#table').DataTable();
+    });
     </script>
 
-    <?php
-        $link=mysqli_connect("localhost","root","12345678","sa");
-        $sql="select user_email,user_name,user_phone from user where user_admin='user'";
-        $rs=mysqli_query($link,$sql);
-        
-        if(!$link){
-          echo "連結失敗".mysqli_connect_error();
-        }
-    ?>
 
     
 <!--
@@ -55,13 +42,24 @@ TemplateMo 559 Zay Shop
 https://templatemo.com/tm-559-zay-shop
 
 -->
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </head>
 
+<?php 
+    session_start();
+    $user_name = $_SESSION['user_name'];
+
+    $link=mysqli_connect("localhost","root","12345678","sa");
+    if(!$link){
+        echo "連結失敗".mysqli_connect_error();
+    }
+    $sql="select user_email, user_name, user_phone from user where user_admin='user' ";
+    $rs=mysqli_query($link,$sql)
+
+
+?>
 <body>
+   
+
     <!-- Header -->
     <nav class="navbar navbar-expand-lg navbar-light shadow">
         <div class="container d-flex justify-content-between align-items-center">
@@ -157,7 +155,7 @@ https://templatemo.com/tm-559-zay-shop
         </div>
     </nav>
     <!-- Close Header -->
-    
+
     <!-- Modal -->
     <div class="modal fade bg-white" id="templatemo_search" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-lg" role="document">
@@ -175,30 +173,84 @@ https://templatemo.com/tm-559-zay-shop
         </div>
     </div>
 
-    <div >
-      <br>
-      <!-- row -->
-      <div style="width:100%;">
-        <table id="管理帳號" class="table table-hover display" cellspacing="0" width="100%">
-          <thead>
-            <tr style="text-align: center;">
-              <th>帳號</th>
-              <th>姓名</th>
-              <th>連絡電話</th>
-            </tr>
-          </thead>
-          <tbody align="center" >
-          <?php
-            while($record=mysqli_fetch_row($rs)){
-                echo "<tr>";
-                for($i=0;$i<count($record);$i++){
-                    echo "<td>$record[$i]</td>";
-                }
-                echo"</tr>";
-            } 
-           ?>
-        </table>
-      </div>
 
-    </div>
+
+   
+
+
+    <!-- Start Categories of The Month -->
+    <section class="container py-5">
+        <div class="row text-center pt-3">
+            <div class="col-lg-6 m-auto">
+                <h3>帳號管理</h3>
+            </div>
+        </div>
+        <div align="center">
+        <div width="100%">
+            <table id="table" width="100%" class="table table-hover display">
+                <thead>
+                    <tr>
+                        <th>使用者帳號</th>
+                        <th>姓名</th>
+                        <th>連絡電話</th>
+                    </tr>
+                </thead>
+                <tbody align="center">
+                    <?php
+                    while($record=mysqli_fetch_row($rs)){?>
+                        <tr>
+                            <?php
+                                for($i=0;$i<count($record);$i++){?>
+                                    <td><?php echo $record[$i]; ?></td>
+
+                            <?php } ?>
+                        </tr>
+                    <?php } ?> 
+                </tbody>
+            </table>
+
+
+
+            
+        </div>
+
+
+        
+    </section>
+    <!-- End Categories of The Month -->
+    
+
+   
+
+   
+    <!-- Start Footer -->
+    <footer class="bg-dark" id="tempaltemo_footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 pt-5">
+                    <h2 class="h2 text-success border-bottom  border-light logo">輔大失物招領專區</h2>
+                    <ul class="list-unstyled text-light footer-link-list">
+                        <li>
+                            <i class="fas fa-map-marker-alt fa-fw"></i>
+                            242新北市新莊區中正路510號<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;學務處生活輔導組  <a class="text-decoration-none" href="tel:02-29053100">02-29053100</a><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;進修部生活輔導組  <a class="text-decoration-none" href="tel:02- 29052246">02- 29052246</a><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;軍訓室  <a class="text-decoration-none" href="tel:02-29052885">02-29052885</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <!--footer 最下面那個深色框框-->
+        <div class="w-100 bg-black py-3">
+            <div class="container"> 
+            </div>
+        </div>
+    </footer>
+    <!-- End Footer -->
+    <!-- Start Script -->
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/templatemo.js"></script>
+    <script src="assets/js/custom.js"></script>
+    <!-- End Script -->
 </body>
+
+</html>
