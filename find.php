64fd51f130b95ae3a6f93de36cd20d899ab878c0
Hsinnii <ginny0312@gmail.com>
2022-05-29 13:13:39 +0800

    0529

--- find.php
+++ find.php
@@ -8,7 +8,7 @@
 <html lang="en">
  
 <head>
-    <title>Zay Shop eCommerce HTML CSS Template</title>
+    <title>輔大遺失物管理系統</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
